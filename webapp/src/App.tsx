38eebfaa8ae87e98ae11ade63192a7e7fb7e9987
uo277891 <uo277891@uniovi.es>
2023-02-09 13:43:36 +0100

    Navegacion basica completada

--- webapp/src/App.tsx
+++ webapp/src/App.tsx
@@ -3,14 +3,23 @@ import Home from "./pages/Home"
 import InicioSesion from "./pages/Login"
 import NavBar from "./components/NavBar";
 import Footer from './components/Footer';
+import {BrowserRouter as Router, Routes, Route} from 'react-router-dom'
 
 const App = () => {
 
   return(
     <div className="App">
+      <Router>
 				<NavBar />
-          <InicioSesion></InicioSesion>
+          <Routes>
+            <Route path='/' element={<Home/>}/>
+            <Route path='/login' element={<InicioSesion/>}/>
+            <Route path='/register' element={<Home/>}/>
+            <Route path='/aboutSocialfs' element={<Home/>}/>
+            <Route path='/follow' element={<Home/>}/>
+          </Routes>
 				<Footer />
+      </Router>
 		</div>
   );
 }
--- webapp/src/components/NavBar.tsx
+++ webapp/src/components/NavBar.tsx
@@ -7,15 +7,29 @@ import Typography from '@mui/material/Typography';
 import Menu from '@mui/material/Menu';
 import MenuIcon from '@mui/icons-material/Menu';
 import Container from '@mui/material/Container';
-import Button from '@mui/material/Button';
 import Tooltip from '@mui/material/Tooltip';
 import MenuItem from '@mui/material/MenuItem';
 import Link from "@mui/material/Link";
 import PersonIcon from '@mui/icons-material/Person';
 import iconlogo from "../images/iconLogoBlanco.png";
 
-const pages = ['Sobre SocialFS','Siguiendo'];
+const nombrePagina = ['Sobre SocialFS','Siguiendo'];
+const linkPagina = ['aboutSocialfs', 'follow']
 const settings = ['Perfil', 'Cerrar Sesion'];
+
+var hashmap = new Map();
+
+function agregarPaginas(){
+  for(let i = 0; i < nombrePagina.length; i++){
+    hashmap.set(nombrePagina[i], linkPagina[i]);
+  }
+}
+agregarPaginas();
+
+function linkAsociado (pagina:string){
+  console.log(hashmap.get(pagina))
+  return hashmap.get(pagina)
+}
 
 function ResponsiveAppBar() {
   const [anchorElNav, setAnchorElNav] = React.useState<null | HTMLElement>(null);
@@ -60,40 +74,15 @@ function ResponsiveAppBar() {
             >
               <MenuIcon />
             </IconButton>
-            <Menu
-              id="menu-appbar"
-              anchorEl={anchorElNav}
-              anchorOrigin={{
-                vertical: 'bottom',
-                horizontal: 'left',
-              }}
-              keepMounted
-              transformOrigin={{
-                vertical: 'top',
-                horizontal: 'left',
-              }}
-              open={Boolean(anchorElNav)}
-              onClose={handleCloseNavMenu}
-              sx={{
-                display: { xs: 'block', md: 'none' },
-              }}
-            >
-              {pages.map((page) => (
-                <MenuItem key={page} onClick={handleCloseNavMenu}>
-                  <Typography textAlign="center">{page}</Typography>
-                </MenuItem>
-              ))}
-            </Menu>
           </Box>
           <Box sx={{ flexGrow: 1, display: { xs: 'none', md: 'flex' } }}>
-            {pages.map((page) => (
-              <Button
-                key={page}
-                onClick={handleCloseNavMenu}
-                sx={{ my: 2, color: 'white', display: 'block' }}
-              >
-                {page}
-              </Button>
+          {nombrePagina.map((nombre) => (
+              <Link
+                id={nombre}
+                href={"/" + linkAsociado(nombre)}
+                sx={{ my: 2, color: "#fff", display: "block", pr: 4, pl: 4 }}
+              > {nombre}
+              </Link>
             ))}
           </Box>
 
