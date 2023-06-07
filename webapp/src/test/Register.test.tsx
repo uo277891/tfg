05713d48ffe25891dea60017054a5ad4bc25a640
uo277891 <uo277891@uniovi.es>
2023-06-07 12:57:36 +0200

    Cambio test

--- webapp/src/test/Register.test.tsx
+++ webapp/src/test/Register.test.tsx
@@ -5,9 +5,9 @@ import Register from '../pages/Register';
 test('Renderiza primera página de Register correctamente', () => {
   const {getByText} = render(<Ruta><Register /></Ruta>);
   expect(getByText('Registro')).toBeInTheDocument();
-  expect(getByText('Información personal')).toBeInTheDocument();
+  expect(getByText('Datos')).toBeInTheDocument();
   expect(getByText('Sobre ti')).toBeInTheDocument();
-  expect(getByText('Resultado final')).toBeInTheDocument();
+  expect(getByText('Resumen')).toBeInTheDocument();
   expect(getByText("¿Ya tienes cuenta?, ¡inicia sesión pulsando !")).toBeInTheDocument();
   expect(getByText("Consulta cómo obtener tu ID de Spotify")).toBeInTheDocument();
   expect(screen.getByLabelText("Nombre de usuario *")).toBeInTheDocument();
@@ -37,7 +37,7 @@ test('Renderiza segunda página de Register correctamente', () => {
 
 test('Renderiza tercera página de Register correctamente', () => {
     const {getByText} = render(<Ruta><Register /></Ruta>);
-    fireEvent.click(getByText('Resultado final'));
+    fireEvent.click(getByText('Resumen'));
     expect(getByText('Añade una foto de perfil (opcional):')).toBeInTheDocument();
     fireEvent.click(getByText('Registrarse'));
     expect(getByText('Algún campo está vacío')).toBeInTheDocument();
